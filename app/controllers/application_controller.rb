a2b2d6cf4a04db39c2d4506c5815a618fd3e23f5
Ben Koonse <bkoonse@gmail.com>
2013-10-08 15:14:01 -0700

    less gold mines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,12 @@ class ApplicationController < ActionController::Base
   require 'open-uri'
 
   def index
-    # http://store.apple.com/us/retail/availabilitySearch
     gold   = 'no gold'
     result = JSON.parse(open('http://store.apple.com/us/retail/availabilitySearch?parts.0=ME307LL%2FA&zip=92101').read)
-    result['body']['stores'].each do |s|
+    result['body']['stores'][0..4].each do |s|
       if s['partsAvailability']['ME307LL/A']['pickupDisplay'] != 'unavailable'
         gold = 'give me the gold: ' + s['storeDisplayName'] + ' ' + s['phoneNumber']
-        Rails.logger.debug "\n\n #{gold} \n\n"
         GoldMailer.gold(gold).deliver
-        break
       end
     end
     render text: gold
