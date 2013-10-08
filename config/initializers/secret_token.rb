adfc6875534a4934107acd51616d96f0506d1469
Ben Koonse <bkoonse@gmail.com>
2013-10-07 19:16:32 -0700

    maybe its a crackhead who got on the wrong stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ilovegold::Application.config.secret_key_base = 'd7343cb6f234dcf1d8a5e413d2cd02b3ec75e4cd725d0f50a06d819270bfb2c558608b16326f094c6ab418eb0f00f6c7bc8c4b9cc34d0e691529ae3aa8ca21d7'
